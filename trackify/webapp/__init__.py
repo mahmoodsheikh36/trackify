ad1719145fe6dc1682e10215d7010a28e2999eae
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-03 11:57:05 +0300

    refactor, add home blueprint, use the new config module

--- trackify/webapp/__init__.py
+++ trackify/webapp/__init__.py
@@ -2,35 +2,27 @@ from flask import Flask, request, g
 import os
 
 from trackify.db.classes import MusicProvider, Request
+from trackify.config import Config
 
-def create_app(test_config=None):
+def create_app():
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_mapping(
-        SECRET_KEY='dev', # shouldnt be used in production!
+        SECRET_KEY=Config.secret_key, # shouldnt be used in production!
     )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    def terminate(e=None):
+        if 'music_provider' in g:
+            g.music_provider.close()
 
     @app.before_request
     def before_request():
         if not 'music_provider' in g:
-            g.music_provider = MusicProvider(app.config['DATABASE_USER'],
-                                             app.config['DATABASE_PASSWD'],
-                                             host=app.config['DATABASE_HOST'],
-                                             database=app.config['DATABASE_NAME'])
-            self.teardown_appcontext(g.music_provider.close)
+            g.music_provider = MusicProvider(Config.database_user,
+                                             Config.database_password,
+                                             Config.database,
+                                             Config.database_host)
+            app.teardown_appcontext(terminate)
         db_request = Request(request)
         g.music_provider.add_request(db_request)
 
@@ -40,6 +32,9 @@ def create_app(test_config=None):
         r.headers["Expires"] = "0"
         r.headers['Cache-Control'] = 'public, max-age=0'
         return r
+
+    from trackify.webapp.home import bp as home_bp
+    app.register_blueprint(home_bp)
 
     return app
 
