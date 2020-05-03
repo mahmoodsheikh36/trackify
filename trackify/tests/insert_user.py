56214d3e695d7a51e365b79d544c5e28005df42c
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-03 10:07:47 +0300

    first test, insert a user to database, successful

---
+++ trackify/tests/insert_user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from trackify.utils import current_time, generate_id
+from trackify.db.classes import MusicProvider, User
+
+def insert_user(username, password, email=None):
+    user_id = generate_id()
+    time_added = current_time()
+    user = User(user_id, username, password, email, time_added)
+    mp = MusicProvider('mahmooz', 'test', database='trackify')
+    mp.add_user(user)
+
+if __name__ == '__main__':
+    insert_user('test', 'test', None)
