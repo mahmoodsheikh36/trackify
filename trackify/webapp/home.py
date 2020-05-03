2bfd88306d92f6f42bbf19ae935ec751150ecaa3
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-03 11:57:40 +0300

    home blueprint

---
+++ trackify/webapp/home.py
@@ -0,0 +1,18 @@
+import functools
+from flask import (
+    Blueprint, render_template
+)
+
+bp = Blueprint('home', __name__, url_prefix='/')
+
+@bp.route('', methods=('GET',))
+def index():
+    return render_template('index.html')
+
+@bp.route('index', methods=('GET',))
+def index_page():
+    return index()
+
+@bp.route('/home', methods=('GET',))
+def home():
+    return index()
