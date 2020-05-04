b76e7f216b0b105d768e6eb6bb0f41b4ce2fbf53
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-04 14:23:37 +0300

    add uri_encode method

--- trackify/utils.py
+++ trackify/utils.py
@@ -1,8 +1,12 @@
 import uuid
 import time
+import urllib.parse
 
 def generate_id():
     return str(uuid.uuid4())
 
 # time since the unix epoch in milliseconds
 current_time = lambda: int(round(time.time() * 1000))
+
+def uri_encode(text):
+    return urllib.parse.quote(text)
