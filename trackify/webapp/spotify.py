49fc5c95f8eeb42459306333090168cf25a4bc32
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-04 19:14:24 +0300

    spotify user authentication now works "perfectly"

--- trackify/webapp/spotify.py
+++ trackify/webapp/spotify.py
@@ -1,12 +1,14 @@
 import functools
-from flask import Blueprint, render_template, g, redirect
+from flask import Blueprint, render_template, g, redirect, request, url_for
 
-from trackify.utils import uri_encode
+from trackify.utils import uri_encode, generate_id, current_time
+from trackify.webapp.auth import login_required
+from trackify.db.classes import AuthCode
 
-bp = Blueprint('/spotify')
+bp = Blueprint('spotify', __name__, url_prefix='/spotify')
 
-@requires_login
-@bp.route('/spotify/auth', methods=('GET',))
+@login_required
+@bp.route('/auth', methods=('GET',))
 def spotify_auth():
     spotify_auth_url = "https://accounts.spotify.com/authorize" +\
         "?response_type=code" +\
@@ -15,4 +17,16 @@ def spotify_auth():
         '&redirect_uri={}'.format(uri_encode(g.spotify_client.redirect_uri))
     return redirect(spotify_auth_url)
 
-@bp.route('/spotify/callback', methods=('GET',))
+@login_required
+@bp.route('/callback', methods=('GET',))
+def callback():
+    if g.music_provider.get_user_auth_code(g.user):
+        return '' # in this case we know someone is trying to abuse the backend
+    auth_code = AuthCode(generate_id(), request.args['code'],
+                         g.user, current_time())
+    g.music_provider.add_auth_code(auth_code)
+    refresh_token, access_token = g.spotify_client.fetch_refresh_token(auth_code)
+    if access_token and refresh_token:
+        g.music_provider.add_refresh_token(refresh_token)
+        g.music_provider.add_access_token(access_token)
+    return redirect(url_for('home.home'))
