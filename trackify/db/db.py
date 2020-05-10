5438012149a7aebeef8088ad8f78fdb0a9787af1
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-10 15:54:32 +0300

    typo in sql statement, had typed id instead of user_id

--- trackify/db/db.py
+++ trackify/db/db.py
@@ -317,5 +317,7 @@ class DBProvider:
 
     def user_has_plays(self, user_id):
         c = self.cursor()
-        c.execute('SELECT id FROM plays WHERE id = %s LIMIT 1', (user_id,))
+        c.execute('SELECT id FROM plays WHERE user_id = %s LIMIT 1', (user_id,))
+        a = c.fetchone()
+        print(a)
         return c.fetchone() is not None
