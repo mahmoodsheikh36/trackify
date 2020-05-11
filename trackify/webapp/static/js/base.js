bfaba430e87cc38a1b7dae8fe36a582ab856f0ba
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-11 21:04:36 +0300

    the navbar at the top now changes the active tab according to the page, before this commit the Home tab was selected all the time

--- trackify/webapp/static/js/base.js
+++ trackify/webapp/static/js/base.js
@@ -11,3 +11,19 @@ function navbarFunction() {
         x.className = "topnav"
     }
 }
+
+window.onload = function() {
+    // the element that contains the name of the tab in the navbar to be selected
+    let elementWithValue = $('#navbar_value')
+    if (elementWithValue) {
+        let value = elementWithValue.value
+        let navbar = $('#topnav')
+        for (let i = 0; i < navbar.children.length; ++i) {
+            navbarTab = navbar.children[i]
+            if (navbarTab.innerHTML === value) {
+                console.log(value)
+                navbarTab.className += 'active'
+            }
+        }
+    }
+}
