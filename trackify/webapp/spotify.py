7cf14a616d887934e58b0ffe5c043b5291c3b128
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2020-05-19 07:29:46 +0300

    added controls to the data template

--- trackify/webapp/spotify.py
+++ trackify/webapp/spotify.py
@@ -38,41 +38,111 @@ def callback():
 @bp.route('/data', methods=('GET',))
 @login_required
 def data():
+    entry_limit = request.args.get('entry_limit')
+    hrs_limit = request.args.get('time_limit')
+    sort_by = request.args.get('sort_by')
+    display_tracks = request.args.get('display_tracks')
+    display_artists = request.args.get('display_artists')
+    display_albums = request.args.get('display_albums')
+
+    if not display_tracks and not display_artists and not display_albums:
+        display_tracks, display_artists = True, True
+
+    if not entry_limit:
+        entry_limit = 50
+    if not hrs_limit:
+        hrs_limit = 24 * 7
+    if not sort_by:
+        sort_by = 'time_listened'
+    elif sort_by != 'time_listened' and sort_by != 'time_added':
+        return ''
+    try:
+        hrs_limit = int(hrs_limit)
+        entry_limit = int(entry_limit)
+    except:
+        return ''
+    if entry_limit > 500:
+        entry_limit = 500
+
+    if hrs_limit == 0:
+        begin_time = None # the listened_ms function will handle it correctly
+    else:
+        begin_time = current_time() - hrs_limit * 3600 * 1000
+
     artists, albums, tracks, plays = g.music_provider.get_user_data(g.user)
-    for play in plays.values():
-        track = tracks[play.track.id]
-        if hasattr(track, 'listened_ms'):
-            track.listened_ms += play.listened_ms()
-        else:
-            track.listened_ms = play.listened_ms()
 
-    for album in albums.values():
-        for track in album.tracks:
-            if hasattr(album, 'listened_ms'):
-                album.listened_ms += track.listened_ms
+    if sort_by == 'time_listened':
+        for play in plays.values():
+            if hasattr(play.track, 'listened_ms'):
+                play.track.listened_ms += play.listened_ms(begin_time)
             else:
-                album.listened_ms = track.listened_ms
+                play.track.listened_ms = play.listened_ms(begin_time)
+            if play.track.listened_ms > 0:
+                play.track.should_be_displayed = True
 
-    for artist in artists.values():
-        for album in artist.albums:
-            if hasattr(album, 'listened_ms'):
-                if hasattr(artist, 'listened_ms'):
-                    artist.listened_ms += album.listened_ms
+        for album in albums.values():
+            for track in album.tracks:
+                if hasattr(album, 'listened_ms'):
+                    album.listened_ms += track.listened_ms
                 else:
-                    artist.listened_ms = album.listened_ms
+                    album.listened_ms = track.listened_ms
+                if album.listened_ms > 0:
+                    album.should_be_displayed = True
 
-    LIMIT = 100
+        for artist in artists.values():
+            for album in artist.albums:
+                if hasattr(album, 'listened_ms'):
+                    if hasattr(artist, 'listened_ms'):
+                        artist.listened_ms += album.listened_ms
+                    else:
+                        artist.listened_ms = album.listened_ms
+                    if artist.listened_ms > 0:
+                        artist.should_be_displayed = True
+    elif sort_by == 'time_added':
+        for play in plays.values():
+            if hasattr(play.track, 'time_added'):
+                if play.track.time_added > play.time_started:
+                    play.track.time_added = play.time_started
+            else:
+                play.track.time_added = play.time_started
+                play.track.should_be_displayed = True
+
+        for album in albums.values():
+            for track in album.tracks:
+                if hasattr(album, 'time_added'):
+                    if album.time_added > track.time_added:
+                        album.time_added = track.time_added
+                else:
+                    album.time_added = track.time_added
+                album.should_be_displayed = True
+
+        for artist in artists.values():
+            for album in artist.albums:
+                if hasattr(album, 'time_added'):
+                    if hasattr(artist, 'time_added'):
+                        if artist.time_added > album.time_added:
+                            artist.time_added = album.time_added
+                    else:
+                        artist.time_added = album.time_added
+                    artist.should_be_displayed = True
 
     def compare(music_obj1, music_obj2):
-        if not hasattr(music_obj1, 'listened_ms'):
-            return False
-        if not hasattr(music_obj2, 'listened_ms'):
-            return True
-        return music_obj1.listened_ms > music_obj2.listened_ms
+        if sort_by == 'time_listened':
+            if not hasattr(music_obj1, 'listened_ms'):
+                return False
+            if not hasattr(music_obj2, 'listened_ms'):
+                return True
+            return music_obj1.listened_ms > music_obj2.listened_ms
+        elif sort_by == 'time_added':
+            if not hasattr(music_obj1, 'time_added'):
+                return False
+            if not hasattr(music_obj2, 'time_added'):
+                return True
+            return music_obj1.time_added > music_obj2.time_added
 
-    top_tracks = get_largest_elements(list(tracks.values()), LIMIT, compare)
-    top_albums = get_largest_elements(list(albums.values()), LIMIT, compare)
-    top_artists = get_largest_elements(list(artists.values()), LIMIT, compare)
+    top_tracks = get_largest_elements(list(tracks.values()), entry_limit, compare)
+    top_albums = get_largest_elements(list(albums.values()), entry_limit, compare)
+    top_artists = get_largest_elements(list(artists.values()), entry_limit, compare)
     
     return render_template('data.html',
                            top_tracks=top_tracks,
@@ -80,7 +150,15 @@ def data():
                            top_artists=top_artists,
                            mins_from_ms=mins_from_ms,
                            hrs_from_ms=hrs_from_ms,
-                           secs_from_ms=secs_from_ms)
+                           secs_from_ms=secs_from_ms,
+                           entry_limit=entry_limit,
+                           sort_by=sort_by,
+                           hrs_limit=hrs_limit,
+                           hasattr=hasattr,
+                           current_time=current_time,
+                           display_albums=display_albums,
+                           display_artists=display_artists,
+                           display_tracks=display_tracks)
 
 @bp.route('/top_users', methods=('GET',))
 def top_users():
